406aeaae87cd751cde758e4f3c6b619d305ee9bc
hayabusa <new.web.hyt8810@gmail.com>
2021-05-08 23:14:36 +0900

    free_list.c , add testcase

--- dest/real_tests/real_tests.h
+++ dest/real_tests/real_tests.h
@@ -1,32 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   real_tests.h                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hida <hida@student.42tokyo.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/08 18:44:22 by hida              #+#    #+#             */
-/*   Updated: 2021/05/08 19:36:33 by apple            ###   ########.fr       */
+/*   Created: 2021/05/08 23:04:23 by hida              #+#    #+#             */
+/*   Updated: 2021/05/08 23:04:26 by hida             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../framework/libunit.h"
-#include "real_tests.h"
+#ifndef LIBUNIT_H
+#define LIBUNIT_H
 
-int	main(void)
-{
-	t_result	result;
-	// スタートの合図の出力
-	put_start("libft");
+int strlen_normal_test(void);
 
-	// 初期化
-	data_init(&result);
-
-	// ランチャーの実行
-	// launcher(result);
-	// launcher(result);
-	// launcher(result);
-
-	print_result("All", result.success_num, result.total_num);
-	return (0);
-}
+#endif
